7e98eecdf192b870683ab77d0dd587ef2f783763
Joaquin <joaquindamianseita@gmail.com>
2021-06-18 01:27:30 -0300

    Front Patch

--- client/src/components/misc/Hero-admin.js
+++ client/src/components/misc/Hero-admin.js
@@ -1,36 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import PostAdd from "../posts/PostAdd";
 
-const Heroadmin = () => (
-  <div className="text-center hero">
-    <img
-      className="mb-3 app-logo"
-      src={process.env.PUBLIC_URL + "/images/blog.svg"}
-      alt="Blog logo"
-      width="120"
-    />
-    <h1 className="mb-4">Admin Mode</h1>
-    <div className="d-flex justify-content-center">
-      <p className="lead" style={{ width: 500, textAlign: "center" }}>
-        Hola administrador, abajo vas a encontrar un botón que te permite
-        publicar posteos nuevos, sin embargo solo los usuarios autorizados
-        podrán hacer uso de esta función. Para autorizarte mandar un mail acá:
-        <a href="mailto:joaquindamianseita@gmail.com">
-          joaquindamianseita@gmail.com
-        </a>
-      </p>
+function Heroadmin() {
+  const [showAdd, setShowAdd] = useState(false);
+
+  function handleCloseAdd() {
+    setShowAdd(false);
+  }
+  function handleShowAdd() {
+    setShowAdd(true);
+  }
+
+  return (
+    <div>
+      <div>
+        <PostAdd isOpen={showAdd} handleCloseAdd={handleCloseAdd}></PostAdd>
+      </div>
+
+      <div className="text-center hero">
+        <img
+          className="mb-3 app-logo"
+          src={process.env.PUBLIC_URL + "/images/blog.svg"}
+          alt="Blog logo"
+          width="120"
+        />
+        <h1 className="mb-4">Admin Mode</h1>
+        <div className="d-flex justify-content-center">
+          <p className="lead" style={{ width: 500, textAlign: "center" }}>
+            Hola administrador, abajo vas a encontrar un botón que te permite
+            publicar posteos nuevos, sin embargo solo los usuarios autorizados
+            podrán hacer uso de esta función. Para autorizarte mandar un mail
+            acá:
+            <a href="mailto:joaquindamianseita@gmail.com">
+              joaquindamianseita@gmail.com
+            </a>
+          </p>
+        </div>
+
+        <button className="btn btn-dark" onClick={handleShowAdd}>
+          Crear posteo
+        </button>
+      </div>
     </div>
-    <button className="btn btn-dark">
-      <NavLink
-        exact
-        className="nav-link link-light"
-        activeClassName="active"
-        to="/posts/new"
-      >
-        Crear post
-      </NavLink>
-    </button>
-  </div>
-);
+  );
+}
 
 export default Heroadmin;
--- client/src/components/posts/PostAdd.jsx
+++ client/src/components/posts/PostAdd.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import axios from "axios";
 import { addPost } from "../../actions";
+import { Modal } from "react-bootstrap";
 
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function PostAdd(props) {
-  const initialState = { name: "", tel: "", email: "", adress: "" };
+  const initialState = { date: "", title: "", content: "" };
   const [post, setFields] = useState(initialState);
+  const [open, setOpen] = useState(props.isOpen);
   const dispatch = useDispatch();
 
   const { getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    setOpen(props.isOpen);
+  }, [props.isOpen]);
 
   function handleChange(event) {
     setFields({ ...post, [event.target.name]: event.target.value });
@@ -19,7 +25,6 @@ export default function PostAdd(props) {
   function handleCancel() {
     props.history.push("/posts");
   }
-
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -55,48 +60,107 @@ export default function PostAdd(props) {
 
   return (
     <div>
-      <div className="container" style={{width:800}}>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <input
-              type="date"
-              name="date"
-              required
-              value={post.date}
-              onChange={handleChange}
-              className="form-control"
-            />
-            <input
-              type="text"
-              name="title"
-              required
-              value={post.title}
-              onChange={handleChange}
-              className="form-control"
-              placeholder="Título"
-            />
-            <textarea
-              type="text"
-              name="content"
-              required
-              value={post.content}
-              onChange={handleChange}
-              className="form-control"
-              placeholder="Contenido"
-            />
-          </div>
-          <div className="btn-group">
-            <input
-              type="submit"
-              value="Confirmar"
-              className="btn btn-primary"
-            />
-            <button className="btn btn secondary" onClick={handleCancel}>
-              Cancel
-            </button>
-          </div>
-        </form>
-      </div>
+      <Modal
+        show={open}
+        size="lg"
+        onHide={props.handleCloseAdd}
+        backdrop="static"
+        keyboard={false}
+        centered
+      >
+        <Modal.Header>
+          <Modal.Title>Crear Posteo</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                type="date"
+                name="date"
+                required
+                value={post.date}
+                onChange={handleChange}
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                name="title"
+                required
+                value={post.title}
+                onChange={handleChange}
+                className="form-control"
+                placeholder="Título"
+              />
+            </div>
+            <div className="form-group">
+              <textarea
+                type="text"
+                name="content"
+                required
+                value={post.content}
+                onChange={handleChange}
+                className="form-control"
+                placeholder="Contenido"
+              />
+            </div>
+            <div className="btn-group d-flex justify-content-center">
+              <input type="submit" value="Submit" className="btn btn-primary" />
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="btn btn-danger" onClick={props.handleCloseAdd}>
+            Cancelar
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
+
+    // <div>
+    //   <div className="container" style={{ width: 800 }}>
+    //     <form onSubmit={handleSubmit}>
+    //       <div className="form-group">
+    //         <input
+    //           type="date"
+    //           name="date"
+    //           required
+    //           value={post.date}
+    //           onChange={handleChange}
+    //           className="form-control"
+    //         />
+    //         <input
+    //           type="text"
+    //           name="title"
+    //           required
+    //           value={post.title}
+    //           onChange={handleChange}
+    //           className="form-control"
+    //           placeholder="Título"
+    //         />
+    //         <textarea
+    //           type="text"
+    //           name="content"
+    //           required
+    //           value={post.content}
+    //           onChange={handleChange}
+    //           className="form-control"
+    //           placeholder="Contenido"
+    //         />
+    //       </div>
+    //       <div className="btn-group">
+    //         <input
+    //           type="submit"
+    //           value="Confirmar"
+    //           className="btn btn-primary"
+    //         />
+    //         <button className="btn btn secondary" onClick={handleCancel}>
+    //           Cancel
+    //         </button>
+    //       </div>
+    //     </form>
+    //   </div>
+    // </div>
   );
 }
